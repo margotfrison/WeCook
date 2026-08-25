3028ff0b428b9bd6251634dd2afbfa5da943f62f
Margot Frison <frison.margot@laposte.net>
2026-08-25 02:15:54 +0200

    Added module Backend

---
+++ backend/src/main/java/me/margueritedefrize/wecook/back/backend/WeCookBackendApplication.java
@@ -0,0 +1,12 @@
+package me.margueritedefrize.wecook.back.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCookBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(WeCookBackendApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/me/margueritedefrize/wecook/back/backend/WeCookBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package me.margueritedefrize.wecook.back.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeCookBackendApplicationTests {
+
+	@Test
+	void contextLoads() { }
+}
